3543f180871924b36607c08336b9d513aad97009
Santiago825 <santiagoorjuela812@gmail.com>
2022-11-26 01:11:13 -0500

    Mi mensaje

--- proyecto_back/Config/database.js
+++ proyecto_back/Config/database.js
@@ -1,12 +1,18 @@
 const mongoose=require("mongoose");
 
-const host ="localhost";
-const port ="27017";
-const db= "Villaplast";
+// const host ="localhost";
+// const port ="27017";
+// const db= "Villaplast";
 
+const host ="santiago_0";
+const port ="12345";
+const db= "villa.yb49rxn.mongodb.net/?retryWrites=true&w=majority";
 
+// mongodb+srv://santiago_0:<password>@villa.yb49rxn.mongodb.net/?retryWrites=true&w=majority
 exports.mongoConnect=()=>{
-    const mongoStringConnection=`mongodb://${host}:${port}/${db}`;
+    // const mongoStringConnection=`mongodb://${host}:${port}/${db}`;
+    const mongoStringConnection=`mongodb+srv://${host}:${port}@${db}`;
+
     mongoose.connect(mongoStringConnection);
     mongoose.Promise=global.Promise;
     const dbConnection=mongoose.connection;
